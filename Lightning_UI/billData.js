e1fa5a4f9dc0340bbdfbfbd2afab266ff614b62a
Cédric Theveneau <87996087+CedricTheveneau@users.noreply.github.com>
2023-11-01 18:21:23 +0100

    Added version number + codename, reworked the zip file

---
+++ Lightning_UI/billData.js
@@ -0,0 +1,20 @@
+const billData = [
+  {
+    title: "My other title",
+    summary: "<p>My summary.</p>",
+    tags: "<span class='tag feature'>Tag 2</span>",
+    date: "<p>-&nbsp;&nbsp; November 1, 2023</p>",
+    content:
+      "<p>My first sentence goes here</p><p>And here's a second one !</p><p>And so on...</p><br/><p>My name, my job</p>",
+    img: "../assets/image-removebg-preview.png",
+  },
+  {
+    title: "My title",
+    summary: "<p>My summary</p>",
+    tags: "<span class='tag release'>Tag 1</span>",
+    date: "<p>-&nbsp;&nbsp; November 1, 2023</p>",
+    content:
+      "<p>My first sentence goes here</p><p>And here's a second one !</p><p>And so on...</p><br/><p>My name, my job</p>",
+    img: "../assets/image-removebg-preview.png",
+  },
+];
